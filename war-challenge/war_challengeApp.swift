0f4fb580533dddf0eb658f84e24b530c27175a58
Mehar <mehar@Mehars-MacBook-Air.local>
2021-08-22 06:19:37 +0400

    first commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Mehar on 14/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
